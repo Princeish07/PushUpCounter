0c6185513623682201048fe6ca8f92d7b66a8809
prince <prince@infostride.com>
2024-07-05 18:33:04 +0530

    example added

---
+++ lib/models/push_up_model.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+enum PushUpState { neutral, init, complete }
+
+class PushUpCounter extends Cubit<PushUpState> {
+  PushUpCounter() : super(PushUpState.neutral);
+  int counter = 0;
+
+  void setUpPushupState(PushUpState current) {
+    print("emittedState ${state}");
+    emit(current);
+  }
+
+  void increment() {
+    counter++;
+    print("Counter ${counter}");
+    emit(state);
+  }
+  void reset(){
+    counter = 0;
+    emit(state);
+  }
+}
